4ef8d4082d721186464725579b20e7ee99a0f07a
BluYous <BluYous@outlook.com>
2022-12-24 10:47:08 +0800

    Update install.sh

--- idea-u/src/install.sh
+++ idea-u/src/install.sh
@@ -4,6 +4,8 @@ wget -q $1 -O - | tar -xz
 find . -maxdepth 1 -type d -name * -execdir mv {} /opt/idea-u \;
 sed -i -E "s/# idea.config.path=\\$\{user.home}\/.IntelliJIdea\/config/idea.config.path=\/config\/config/g" /opt/idea-u/bin/idea.properties
 sed -i -E "s/# idea.system.path=\\$\{user.home}\/.IntelliJIdea\/system/idea.system.path=\/config\/system/g" /opt/idea-u/bin/idea.properties
+sed -i -E "s/# idea.plugins.path/idea.plugins.path/g" /opt/idea-u/bin/idea.properties
+sed -i -E "s/# idea.log.path/idea.log.path/g" /opt/idea-u/bin/idea.properties
 mkdir /config
 chown -R 1000:0 /config
 chmod 666 /opt/idea-u/bin/idea64.vmoptions
