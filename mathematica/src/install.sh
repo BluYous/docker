de2628a36504c8690ce24a73fe9bc955b5e1953a
BluYous <BluYous@outlook.com>
2023-01-07 02:53:01 +0000

    fix bug

--- mathematica/src/install.sh
+++ mathematica/src/install.sh
@@ -1,5 +1,5 @@
 set -ex;
-curl -L https://wdm.itsu.eu.org/d/auto/Mathematica/${version}.0/Main/Mathematica_${version}_BNDL_Chinese_LINUX_CN.sh -o /tmp/Mathematica.sh;
+curl -L https://wdm.itsu.eu.org/d/auto/Mathematica/$1.0/Main/Mathematica_$1_BNDL_Chinese_LINUX_CN.sh -o /tmp/Mathematica.sh;
 chmod +x /tmp/Mathematica.sh;
 /tmp/Mathematica.sh -- -silent -verbose
 rm -rf /tmp/Mathematica.sh
