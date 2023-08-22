0206b3f8b7676992b0f71e64c6e0e8ab46fc9d8f
BluYous <BluYous@outlook.com>
2023-08-22 13:24:38 +0000

    refactor

--- rdesktop/src/install.sh
+++ rdesktop/src/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 set -o pipefail
-if [ -z "$1" ]; then
-  echo "static resource host is required!"
+if [ ! -e FILE__STATIC_RESOURCE_HOST ]; then
+  echo "FILE__STATIC_RESOURCE_HOST does not exist!"
   exit 1
 fi
-STATIC_RESOURCE_HOST="$1"
+STATIC_RESOURCE_HOST="$(cat /tmp/FILE__STATIC_RESOURCE_HOST)"
+rm -f /tmp/FILE__STATIC_RESOURCE_HOST
 # Update repositories
 curl -fsSL "https://raw.githubusercontent.com/linuxserver/docker-mods/universal-internationalization/root/etc/s6-overlay/s6-rc.d/init-mod-universal-internationalization-install/run" | bash -
 
@@ -19,8 +20,8 @@ apt-get install -y iputils-ping iproute2
 download_url=$(curl -fsSL --retry 10 --retry-all-errors --retry-connrefused "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release" | jq -r ".IIU[0].downloads.linux.link")
 curl -fsSL "$download_url" | tar -zxC "/opt"
 mv "$(ls -dt "/opt/idea-IU-"*)" "/opt/idea-IU"
-sed -i -E "s/# idea.config.path=\\$\{user.home}\/.IntelliJIdea\/config/idea.config.path=\/config\/config/g" /opt/idea-IU/bin/idea.properties
-sed -i -E "s/# idea.system.path=\\$\{user.home}\/.IntelliJIdea\/system/idea.system.path=\/config\/system/g" /opt/idea-IU/bin/idea.properties
+sed -i -E "s/# idea.config.path=\\$\{user.home}\/.IntelliJIdea\/config/idea.config.path=\/config\/.config\/JetBrains\/IntelliJIdea/g" /opt/idea-IU/bin/idea.properties
+sed -i -E "s/# idea.system.path=\\$\{user.home}\/.IntelliJIdea\/system/idea.system.path=\/config\/.cache\/JetBrains\/IntelliJIdea/g" /opt/idea-IU/bin/idea.properties
 sed -i -E "s/# idea.plugins.path/idea.plugins.path/g" /opt/idea-IU/bin/idea.properties
 sed -i -E "s/# idea.log.path/idea.log.path/g" /opt/idea-IU/bin/idea.properties
 curl -fsSL "$STATIC_RESOURCE_HOST/soft/jetbra.zip" -o "/tmp/jetbra.zip"
