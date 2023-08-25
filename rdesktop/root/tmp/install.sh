1af1f1b0237e7249a8ce5dc4ac9256ed1cc71426
BluYous <BluYous@outlook.com>
2023-08-25 16:09:44 +0800

    fix bug

--- rdesktop/root/tmp/install.sh
+++ rdesktop/root/tmp/install.sh
@@ -8,8 +8,10 @@ fi
 STATIC_RESOURCE_HOST="$(cat "/tmp/FILE__STATIC_RESOURCE_HOST")"
 rm -f "/tmp/FILE__STATIC_RESOURCE_HOST"
 # update LANG and time zone
-echo 'LANG="C.UTF-8"' >>/etc/environment
-echo 'LC_ALL="C.UTF-8"' >>/etc/environment
+{
+  echo 'LANG="C.UTF-8"'
+  echo 'LC_ALL="C.UTF-8"'
+} >>/etc/environment
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 # install internationalization
@@ -51,7 +53,7 @@ curl -fsSL -q "https://www.scootersoftware.com$download_url" -o "/tmp/bcompare.d
 apt-get install -y '/tmp/bcompare.deb'
 
 # GoldenDict
-download_url=$(curl -fsSL --retry 10 --retry-all-errors --retry-connrefused "https://api.github.com/repos/xiaoyifang/goldendict-ng/releases/latest" | jq  -r ".assets[].browser_download_url" | grep "AppImage"  | sed -n '$p')
+download_url=$(curl -fsSL --retry 10 --retry-all-errors --retry-connrefused "https://api.github.com/repos/xiaoyifang/goldendict-ng/releases/latest" | jq -r ".assets[].browser_download_url" | grep "AppImage" | sed -n '$p')
 curl -fsSL -q "$download_url" -o "/tmp/GoldenDict.AppImage"
 chmod +x '/tmp/GoldenDict.AppImage'
 '/tmp/GoldenDict.AppImage' --appimage-extract
@@ -60,7 +62,7 @@ ln -s /usr/lib/x86_64-linux-gnu/nss/* /opt/GoldenDict/usr/lib/
 sed -i '2i export QTWEBENGINE_DISABLE_SANDBOX=1' /opt/GoldenDict/AppRun
 
 # PeaZip
-download_url=$(curl -fsSL --retry 10 --retry-all-errors --retry-connrefused "https://api.github.com/repos/peazip/PeaZip/releases/latest" | jq  -r ".assets[].browser_download_url" | grep -i "Qt.*amd64\.deb")
+download_url=$(curl -fsSL --retry 10 --retry-all-errors --retry-connrefused "https://api.github.com/repos/peazip/PeaZip/releases/latest" | jq -r ".assets[].browser_download_url" | grep -i "Qt.*amd64\.deb")
 curl -fsSL -q "$download_url" -o "/tmp/peazip.deb"
 apt-get install -y libqt5printsupport5 libqt5x11extras5
 apt-get install -y '/tmp/peazip.deb'
@@ -69,9 +71,14 @@ apt-get install -y '/tmp/peazip.deb'
 apt install -y fcitx
 cp /usr/share/applications/fcitx.desktop /etc/xdg/autostart/
 apt install -y libqt5qml5 libqt5quick5 libqt5quickwidgets5 qml-module-qtquick2 libgsettings-qt1
-download_url=$(curl -fsSL --retry 10 --retry-all-errors --retry-connrefused "https://shurufa.sogou.com/linux"  | grep -oE 'https[^"]+amd64\.deb')
+download_url=$(curl -fsSL --retry 10 --retry-all-errors --retry-connrefused "https://shurufa.sogou.com/linux" | grep -oE 'https[^"]+amd64\.deb')
 curl -fsSL -q "$download_url" -o "/tmp/sogoupinyin.deb"
 apt-get install -y '/tmp/sogoupinyin.deb'
+{
+  echo 'GTK_IM_MODULE="fcitx"'
+  echo 'QT_IM_MODULE="fcitx"'
+  echo 'XMODIFIERS="@im=fcitx"'
+} >>/etc/environment
 
 # remove cache
 apt-get autoclean &&
