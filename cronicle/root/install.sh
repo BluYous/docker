1f67f4271704060ea27ed2ab070ab943bdaf5c69
BluYous <BluYous@outlook.com>
2023-10-03 15:20:32 +0800

    add mmdbinspect

--- cronicle/root/install.sh
+++ cronicle/root/install.sh
@@ -7,3 +7,8 @@ apk add --no-cache jq py3-pip qemu-img
 npm install -g bestroutetb he
 pip install csvkit
 bestroutetb --update
+
+# install mmdbinspect
+curl -fsSL "$(curl -fsSL -q "https://api.github.com/repos/maxmind/mmdbinspect/releases/latest" | jq -r ".assets[].browser_download_url" | grep "linux_amd64\.tar\.gz")" | tar -zxC "/tmp" --wildcards mmdbinspect*/mmdbinspect
+mv /tmp/mmdbinspect*/mmdbinspect /usr/local/bin/
+rm -rf /tmp/mmdbinspect*
